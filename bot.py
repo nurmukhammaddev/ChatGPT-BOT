504545541390329d1ec0da9c0b8a5bc526fe7158
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2023-02-10 12:43:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,29 @@
-import os
 import openai
+import os
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from environs import Env
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-token = os.getenv("TOKEN")
+env = Env()
+env.read_env()
+
+
+token = env.str("BOT_TOKEN")
+openai_api_key = os.environ.get('OPENAI_API_KEY')
+
 
 bot = Bot(token)
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Hello! I'm a bot that can talk with you. \n"
+                         "Send me a message and I'll try to answer it as best I can")
 
+
+    
 
 
 @dp.message_handler()
@@ -26,9 +38,6 @@ async def send(message : types.Message):
     presence_penalty=0.6,
     stop=["You:"]
 )
-    wait = await message.answer("I am thinking... ⏳")
     await message.answer(response['choices'][0]['text'])
-    await wait.delete()
-
 
 executor.start_polling(dp, skip_updates=True)
